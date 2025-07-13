18f908ff2c7e7481f682b87f81cec797a604f8c3
armcortex3445 <wlghks5966@naver.com>
2025-07-13 17:02:11 +0900

    feat: update project image

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,7 +9,7 @@ const projectsData: Project[] = [
   {
     title: '고전 작품 퀴즈 서비스',
     description: `예술적인 작품들을 감상하는 것 뿐만 아니라, 퀴즈 컨텐츠로써 즐깁니다.`,
-    imgSrc: '/static/images/project/quiz.png',
+    imgSrc: '/static/images/project/ggrim-quiz.png',
     href: 'https://port-0-ggrim-front-m4i5o7t86f50fb45.sel4.cloudtype.app/',
   },
 ]
